4aaffa929222f8718a28b9457f3705f0969dc76f
Pasteldreamzzzz <gabrielle.paul@patelhs.org>
2021-02-09 04:04:38 +0000

    OH MY GOSH I FINALLY GOT IT IM LITERALLY SO HAPPY

--- Main.java
+++ Main.java
@@ -1,23 +1,24 @@
-class Main {
-  static void happyValentines(String name) {
+public class Main {
+  static void happyValentines(String name, int grams) {
  System.out.println("Hey! " + name + " I love you!!");
+ System.out.println("Congrats " + name + " you have " + grams + " number of Valentines candy grams");
   }
 
   public static void main(String[] args ){
-    happyValentines("Lisa"); 
-    happyValentines("Rosline"); 
-    happyValentines("Stephanie"); 
+    happyValentines("Lisa",14); 
+    happyValentines("Rosline",18); 
+    happyValentines("Stephanie",21); 
   }
 
-  public static void main(String[] args ){
+ 
+ static void myMethod(String name, int grams  ){ 
+    System.out.println("Congrats " + name + " you have " + grams + " number of Valentines candy grams");
+  }
+ 
+ public static void second(String[] args ){
    myMethod("Lisa", 14);
    myMethod("Rosline", 18);
    myMethod("Stephanie", 21);
   }
   
-  
-  
-  static void myMethod(String name, int grams  ){ 
-    System.out.println("Congrats " + name + " you have " + grams + " number of Valentines candy grams");
-  }
 }  
